569f448890a59b20140a34c2d6d72d63052c05d4
Vice-0-0 <664239454@qq.com>
2025-03-27 08:04:24 +0800

    更新

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ app = Flask(__name__)
 # 配置日志
 logging.basicConfig(level=logging.DEBUG)
 
-@app.route('/generate_word', methods=['GET'])  # 修改为 GET 方法
+@app.route('/generate_word', methods=['GET', 'POST'])
 def generate_word():
-    app.logger.debug(f"Request method: {request.method}")
-    app.logger.debug(f"Request URL: {request.url}")
-    app.logger.debug(f"Request data: {request.args}")
-    data = request.args  # 从 request.args 中获取数据
+    if request.method == 'GET':
+        data = request.args
+    else:
+        data = request.form
+    
+    app.logger.debug(f"Received request data: {data}")  # 打印接收到的表单数据
+    
     # 加载 Word 模板
     doc = Document('template.docx')
 
@@ -29,17 +32,13 @@ def generate_word():
     doc.save(file_stream)
     file_stream.seek(0)
 
-    # 连接到 FTP 服务器
-    ftp = ftplib.FTP('192.168.1.26')
-    ftp.login('用户名', '密码')
-
-    # 上传文件
-    ftp.storbinary('STOR output.docx', file_stream)
-
-    # 关闭 FTP 连接
-    ftp.quit()
-
-    return "文件已成功发送到指定电脑"
+    # 返回文件供用户下载
+    return send_file(
+        file_stream,
+        as_attachment=True,
+        download_name='output.docx',
+        mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
